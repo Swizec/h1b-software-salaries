8cf4b0139c29dc59fdfa98df7c107287493219ca
Swizec Teller <swizec@swizec.com>
2015-02-15 19:44:46 +0100

    loading data

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,50 @@
 
+var H1BGraph = React.createClass({
+    loadRawData: function () {
+        var dateFormat = d3.time.format("%m/%d/%Y");
+        d3.csv(this.props.url)
+          .row(function (d) {
+              if (!d['base salary']) {
+                  return null;
+              }
+
+              return {employer: d.employer,
+                      submit_date: dateFormat.parse(d['submit date']),
+                      start_date: dateFormat.parse(d['start date']),
+                      case_status: d['case status'],
+                      job_title: d['job title'],
+                      base_salary: Number(d['base salary']),
+                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
+                      city: d.city,
+                      state: d.state};
+          })
+          .get(function (error, rows) {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              }else{
+                  this.setState({rawData: rows});
+              }
+          }.bind(this));
+    },
+
+    getInitialState: function () {
+        return {raw_data: []};
+    },
+
+    componentDidMount: function () {
+        this.loadRawData();
+    },
+
+    render: function () {
+        return (
+            <svg width={this.props.width} height={this.props.height}>
+            </svg>
+        );
+    }
+});
+
 React.render(
-    <h2>Hello there!</h2>,
-    $("#example")[0]
+    <H1BGraph width="1024" height="768" url="data/h1bs.csv" />,
+    document.getElementById('graph')
 );
