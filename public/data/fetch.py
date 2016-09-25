b6ae68417b1768389ef2601b30cf6d0dad2d64cd
Swizec Teller <swizec@swizec.com>
2016-09-24 21:22:49 -0700

    Bigger better dataset

--- public/data/fetch.py
+++ public/data/fetch.py
@@ -4,10 +4,10 @@
 from pyquery import PyQuery as pq
 import requests, csv
 
-def download(url):
+def download(url, year):
     d = pq(url=url)
 
-    with open("raw.html", "wb") as f:
+    with open("raw-%d.html" % year, "wb") as f:
         r = requests.get(url, stream=True)
         for chunk in r.iter_content(100):
             f.write(chunk)
@@ -15,9 +15,7 @@ def download(url):
     f.closed
 
 def parse():
-    d = pq(filename="raw.html")
-
-    print d.encoding
+    d = pq(filename="raw-2012.html")
 
     def cell(td):
         td = d(td)
@@ -25,16 +23,23 @@ def parse():
           if td.text() else ""
 
 
-    with open("h1bs.csv", "wb") as csvfile:
+    with open("h1bs-2012-2016.csv", "wb") as csvfile:
         writer = csv.writer(csvfile)
 
         header = [th.text.lower() for th in d("thead th")]
         writer.writerow([th.text.lower()
                          for th in d("thead th")])
 
-        for row in d("table tr"):
-            writer.writerow([cell(td) for td in d(row)("td")])
+        for year in xrange(2012, 2017):
+            d = pq(filename="raw-%d.html" % year)
+            print year
+
+            for row in d("table tr"):
+                writer.writerow([cell(td) for td in d(row)("td")])
 
 if __name__ == "__main__":
-    download("http://h1bdata.info/index.php?em=&job=software+*")
-    #parse()
+    #for year in xrange(2012, 2017):
+    #    print year
+        #download("http://h1bdata.info/index.php?em=&job=software+*&year=%d" % year, year)
+
+    parse()
