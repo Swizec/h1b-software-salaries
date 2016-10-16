998042ef24d58c3eb1ff4e7544f119e23feb2fc0
Swizec Teller <swizec@swizec.com>
2016-10-16 14:08:43 -0700

    Cleanup numbers in datasets

---
+++ public/data/cleanup_numbers.py
@@ -0,0 +1,48 @@
+
+import csv, re
+
+def cleanNumber(n):
+    n = int(n.replace('.', '').replace(',', ''))
+
+    if (n < 100):
+        n *= 1000
+
+    if (n < 1000):
+        n *= 10
+
+    if (n < 10000):
+        n *= 10
+
+    return n
+
+def cleanupMedians():
+    with open('county-median-incomes-normalized.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+
+        with open('county-median-incomes.csv', 'wb') as csvoutfile:
+            writer = csv.writer(csvoutfile)
+            writer.writerow(reader.next())
+
+            for row in reader:
+                row[3] = cleanNumber(row[3])
+                row[4] = cleanNumber(row[4])
+                row[5] = cleanNumber(row[5])
+
+                writer.writerow(row)
+
+def cleanupH1Bs():
+    with open('h1bs-2012-2016-final-with-countyid.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+
+        with open('h1bs-2012-2016.csv', 'wb') as csvoutfile:
+            writer = csv.writer(csvoutfile)
+            writer.writerow(reader.next())
+
+            for row in reader:
+                row[2] = cleanNumber(row[2])
+
+                writer.writerow(row)
+
+if __name__ == "__main__":
+    #cleanupMedians()
+    cleanupH1Bs()
