726072941c232748c2594e95c7f63f07172b7e31
Swizec Teller <swizec@swizec.com>
2018-09-01 16:42:54 -0700

    Update for modern dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
