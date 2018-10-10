09ac617a4cec6645722330a1498eb66de672d329
Swizec Teller <swizec@swizec.com>
2018-10-09 23:27:41 -0700

    Plural consistency

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import _ from "lodash";
 
-const cleanIncomes = d => ({
+const cleanIncome = d => ({
     countyName: d["Name"],
     USstate: d["State"],
     medianIncome: Number(d["Median Household Income"]),
@@ -46,7 +46,7 @@ export const loadAllData = (callback = _.noop) => {
     Promise.all([
         d3.json("data/us.json"),
         d3.csv("data/us-county-names-normalized.csv", cleanCounty),
-        d3.csv("data/county-median-incomes.csv", cleanIncomes),
+        d3.csv("data/county-median-incomes.csv", cleanIncome),
         d3.csv("data/h1bs-2012-2018.csv", cleanSalary),
         d3.tsv("data/us-state-names.tsv", cleanUSStateName)
     ]).then(([us, countyNames, medianIncomes, techSalaries, USstateNames]) => {
