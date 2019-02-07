8d5f9c502adf1c60988a1d0a97d4bd20219cbb12
Swizec Teller <swizec@swizec.com>
2019-02-06 21:45:04 -0800

    Update React to version with hooks

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -47,7 +47,7 @@ export const loadAllData = (callback = _.noop) => {
         d3.json("data/us.json"),
         d3.csv("data/us-county-names-normalized.csv", cleanCounty),
         d3.csv("data/county-median-incomes.csv", cleanIncome),
-        d3.csv("data/h1bs-2012-2018.csv", cleanSalary),
+        d3.csv("data/h1bs-2012-2016-shortened.csv", cleanSalary),
         d3.tsv("data/us-state-names.tsv", cleanUSStateName)
     ]).then(([us, countyNames, medianIncomes, techSalaries, USstateNames]) => {
         let medianIncomesMap = {};
