63253e7be1f9131c28a565236f762f297045e193
Swizec Teller <swizec@swizec.com>
2019-02-06 22:00:20 -0800

    Using hooks to update D3 state in medianLine

--- src/components/MedianLine.js
+++ src/components/MedianLine.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as d3 from "d3";
 
 const MedianLine = ({
@@ -11,11 +11,23 @@ const MedianLine = ({
     bottomMargin,
     median
 }) => {
-    const yScale = d3
+    const [{ yScale, line }, setYScale] = useState({});
+
+    // Calculate D3 objects on the smallest possible amount of change
+    // then using local state to update what the outside uses
+    useEffect(() => {
+        const yScale = d3
             .scaleLinear()
             .domain([0, d3.max(data, value)])
-            .range([height - y - bottomMargin, 0]),
-        line = d3.line()([[0, 5], [width, 5]]);
+            .range([height - y - bottomMargin, 0]);
+        const line = d3.line()([[0, 5], [width, 5]]);
+
+        setYScale({ yScale, line });
+    }, [data.length, value, width, height, y, bottomMargin]);
+
+    if (!yScale) {
+        return null;
+    }
 
     const medianValue = median || d3.median(data, value);
 
